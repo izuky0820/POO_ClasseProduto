41ec7c7d7d0bd205575a2b198542359028ab4aa8
Evelyn <1613698@sga.pucminas.br>
2026-08-14 20:44:00 -0300

    Versão 1 da classe Produto

--- src/Projeto_POO/Produto.cs
+++ src/Projeto_POO/Produto.cs
@@ -8,25 +8,46 @@ namespace Projeto_POO {
     public class Produto {
 
         #region atributos
-        // TODO: declarar atributos
+        string _descricao;
+        double _precoCusto, _margemLucro;
 
         #endregion
 
         #region construtores
         public Produto(string desc, double precoCusto, double margemLucro) {
-            //TODO: lógica do construtor
+            if(margemLucro < 10 || margemLucro > 50)
+            {
+                _margemLucro = 10;
+            }
+            if(precoCusto < 0)
+            {
+                precoCusto = 0.1;
+            }
+            if(desc.Length == 0)
+            {
+                desc = "produto sem descrição";
+            }
+
+            _descricao = desc;
+            _precoCusto = precoCusto;
+            _margemLucro = margemLucro;
         }
         #endregion
 
         #region métodos de negócio
         public double ValorVenda() {
             //TODO: calcular valor de venda de acordo com a regra do produto
-            return 0;
+            return _precoCusto * (1 + _margemLucro / 100);
         }
 
         public string NotaDeVenda() {
             //TODO: criar nota de venda formatada de acordo com a regra do produto
-            return "";
+            string nota = "NOTA DE VENDA";
+            nota += "\n####################";
+            nota += $"\nDescrição: {_descricao}";
+            nota += $"\nPreço: {ValorVenda():C2}";
+            nota += "\n####################";
+            return nota ;
         }
         #endregion
     }
